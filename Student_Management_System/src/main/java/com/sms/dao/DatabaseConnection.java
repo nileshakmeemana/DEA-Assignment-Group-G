1e460cf70248369eaf4475cd62f8588e6ba20f8a
Jithendra-ayesh <jithendraayesh25@gmail.com>
2025-05-13 18:47:45 +0530

    Update DatabaseConnection.java

--- Student_Management_System/src/main/java/com/sms/dao/DatabaseConnection.java
+++ Student_Management_System/src/main/java/com/sms/dao/DatabaseConnection.java
@@ -4,10 +4,24 @@
  */
 package com.sms.dao;
 
+import java.sql.*;
 /**
  *
  * @author Nilesh
  */
+
+
 public class DatabaseConnection {
-    
+    private static Connection conn;
+
+    private static final String URL = "jdbc:mysql://localhost:3306/student_management_system";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        if (conn == null || conn.isClosed()) {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+        }
+        return conn;
+    }
 }
